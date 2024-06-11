a324b3d5232f2353f84578533eab5eb970e16df9
Pranav <pranav123panna@gmail.com>
2024-06-11 11:08:53 +0530

    commit2

--- src/main/java/com/neo/model/Designation.java
+++ src/main/java/com/neo/model/Designation.java
@@ -3,13 +3,21 @@ import java.util.*;
 
 public class Designation {
     private String designation;
+    private int count;
+    private int grade;
     public void getDesignation()
     {
         Scanner sc=new Scanner(System.in);
         System.out.println("Designation:");
         designation=sc.nextLine();
+        System.out.println("Count:");
+        count=sc.nextInt();
+        System.out.println("Grade:");
+        grade=sc.nextInt();
     }
     public void printDesignation() {
         System.out.println("  Designation: " + designation);
+        System.out.println("  Count: " + count);
+        System.out.println("  Grade: " + grade);
     }
 }
